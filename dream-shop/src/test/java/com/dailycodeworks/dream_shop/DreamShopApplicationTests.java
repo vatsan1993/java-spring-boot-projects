67a55ae0bfc51dcc7dcdebbf9e556be6b0b51069
vatsan1993 <sozhavaramsrivatsan@hotmail.com>
2025-03-19 11:31:39 +0530

    Project setup

---
+++ dream-shop/src/test/java/com/dailycodeworks/dream_shop/DreamShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailycodeworks.dream_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
